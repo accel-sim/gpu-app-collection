8c0b6a5ab477887fee3fd734e3aa9353cf31936a
Tim Rogers <timrogers@purdue.edu>
2020-04-26 11:57:25 -0400

    Turing build changes

--- src/cuda/common/common.mk
+++ src/cuda/common/common.mk
@@ -24,7 +24,7 @@ INCLUDES += -I$(NVIDIA_COMPUTE_SDK_LOCATION)/../4.2/C/common/inc
 ADDITIONAL_LIBS := -L$(NVIDIA_COMPUTE_SDK_LOCATION)/../4.2/C/lib -lcutil_x86_64
 
 # Add new SM Versions here as devices with new Compute Capability are released
-SM_VERSIONS   := 10 11 12 13 20 21 30 50 60 62
+SM_VERSIONS   := 10 11 12 13 20 21 30 50 60 62 70 75
 
 CUDA_INSTALL_PATH ?= /home/tgrogers-raid/a/common/cuda-4.2
 
@@ -157,6 +157,8 @@ GENCODE_SM35 ?= -gencode=arch=compute_35,code=\"sm_35,compute_35\"
 GENCODE_SM50 ?= -gencode=arch=compute_50,code=\"sm_50,compute_50\"
 GENCODE_SM60 ?= -gencode=arch=compute_60,code=\"sm_60,compute_60\"
 GENCODE_SM62 ?= -gencode=arch=compute_62,code=\"sm_62,compute_62\"
+GENCODE_SM70 ?= -gencode=arch=compute_70,code=\"sm_70,compute_70\"
+GENCODE_SM75 ?= -gencode=arch=compute_75,code=\"sm_75,compute_75\"
 
 CXXFLAGS  += $(CXXWARN_FLAGS) $(CXX_ARCH_FLAGS)
 CFLAGS    += $(CWARN_FLAGS) $(CXX_ARCH_FLAGS)
@@ -430,11 +432,11 @@ $(OBJDIR)/%.cpp.o : $(SRCDIR)%.cpp $(C_DEPS) makedirectories
 
 # Default arch includes gencode for sm_10, sm_20, sm_30, and other archs from GENCODE_ARCH declared in the makefile
 $(OBJDIR)/%.cu.o : $(SRCDIR)%.cu $(CU_DEPS) makedirectories
-	$(VERBOSE)$(NVCC) $(GENCODE_SM10) $(GENCODE_SM13) $(GENCODE_ARCH) $(GENCODE_SM20) $(GENCODE_SM30) $(GENCODE_SM35) $(GENCODE_SM50) $(GENCODE_SM60) $(GENCODE_SM62) $(NVCCFLAGS) $(SMVERSIONFLAGS) -o $@ -c $<
+	$(VERBOSE)$(NVCC) $(GENCODE_SM10) $(GENCODE_SM13) $(GENCODE_ARCH) $(GENCODE_SM20) $(GENCODE_SM30) $(GENCODE_SM35) $(GENCODE_SM50) $(GENCODE_SM60) $(GENCODE_SM62) $(GENCODE_SM70) $(GENCODE_SM75) $(NVCCFLAGS) $(SMVERSIONFLAGS) -o $@ -c $<
 
 # Default arch includes gencode for sm_10, sm_20, sm_30, and other archs from GENCODE_ARCH declared in the makefile
 $(CUBINDIR)/%.cubin : $(SRCDIR)%.cu cubindirectory makedirectories
-	$(VERBOSE)$(NVCC) $(GENCODE_SM10) $(GENCODE_SM13) $(GENCODE_ARCH) $(GENCODE_SM20) $(GENCODE_SM30) $(GENCODE_SM35) $(GENCODE_SM50) $(GENCODE_SM60) $(GENCODE_SM62) $(CUBIN_ARCH_FLAG) $(NVCCFLAGS) $(SMVERSIONFLAGS) -o $@ -cubin $<
+	$(VERBOSE)$(NVCC) $(GENCODE_SM10) $(GENCODE_SM13) $(GENCODE_ARCH) $(GENCODE_SM20) $(GENCODE_SM30) $(GENCODE_SM35) $(GENCODE_SM50) $(GENCODE_SM60) $(GENCODE_SM62) $(GENCODE_SM70) $(GENCODE_SM75) $(CUBIN_ARCH_FLAG) $(NVCCFLAGS) $(SMVERSIONFLAGS) -o $@ -cubin $<
 
 $(PTXDIR)/%.ptx : $(SRCDIR)%.cu ptxdirectory makedirectories
 	$(VERBOSE)$(NVCC) $(CUBIN_ARCH_FLAG) $(NVCCFLAGS) $(SMVERSIONFLAGS) -o $@ -ptx $<
--- src/cuda/polybench-gpu-1.0/CUDA/common.mk
+++ src/cuda/polybench-gpu-1.0/CUDA/common.mk
@@ -7,8 +7,10 @@ GENCODE_SM35 ?= -gencode=arch=compute_35,code=\"sm_35,compute_35\"
 GENCODE_SM50 ?= -gencode=arch=compute_50,code=\"sm_50,compute_50\"
 GENCODE_SM60 ?= -gencode=arch=compute_60,code=\"sm_60,compute_60\"
 GENCODE_SM62 ?= -gencode=arch=compute_62,code=\"sm_62,compute_62\"
+GENCODE_SM70 ?= -gencode=arch=compute_70,code=\"sm_70,compute_70\"
+GENCODE_SM75 ?= -gencode=arch=compute_75,code=\"sm_75,compute_75\"
 
 all:
-	nvcc -O3 ${GENCODE_SM10} ${GENCODE_SM13} ${GENCODE_SM20} ${GENCODE_SM30} ${GENCODE_SM35} ${GENCODE_SM35} ${GENCODE_SM50} ${GENCODE_SM50} ${GENCODE_SM60} ${GENCODE_SM62} ${NVCC_ADDITIONAL_ARGS} ${CUFILES} -o ${EXECUTABLE} 
+	nvcc -O3 ${GENCODE_SM10} ${GENCODE_SM13} ${GENCODE_SM20} ${GENCODE_SM30} ${GENCODE_SM35} ${GENCODE_SM35} ${GENCODE_SM50} ${GENCODE_SM50} ${GENCODE_SM60} ${GENCODE_SM62} ${GENCODE_SM70} ${GENCODE_SM75} ${NVCC_ADDITIONAL_ARGS} ${CUFILES} -o ${EXECUTABLE} 
 clean:
 	rm -f *~ *.exe
