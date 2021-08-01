d6bad7c4b05b450e009e49d4a776121cb6aaae23
Vijay Kandiah <vkz4947@peroni.cs.northwestern.edu>
2021-07-31 19:08:16 -0500

    Updating ubench LICENSES

--- src/cuda/accelwattch-ubench/tensor_benchmarks/TENSOR/tensorcore.cu
+++ src/cuda/accelwattch-ubench/tensor_benchmarks/TENSOR/tensorcore.cu
@@ -1,3 +1,18 @@
+/*
+ * Copyright 1993-2017 NVIDIA Corporation.  All rights reserved.
+ *
+ * Please refer to the NVIDIA end user license agreement (EULA) associated
+ * with this source code for terms and conditions that govern your use of
+ * this software. Any use, reproduction, disclosure, or distribution of
+ * this software and related documentation outside the terms of the EULA
+ * is strictly prohibited.
+ *
+ */
+/********************************************************************
+ *      Modified by:
+ * Vijay Kandiah, Northwestern University
+ ********************************************************************/
+
 #include <stdio.h>
 #include <curand.h>
 #include <cublas_v2.h>
