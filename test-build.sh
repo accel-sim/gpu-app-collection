ec83b39ae82d027f91020059f8fa6247057255f4
tgrogers <tgrogers@purdue.edu>
2025-01-22 14:57:02 -0500

    setting env

--- test-build.sh
+++ test-build.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -n "$CUDA_INSTALL_PATH" ]; then
+»   echo "ERROR ** Install CUDA Toolkit and set CUDA_INSTALL_PATH.";
+»   exit;
+fi
+
+#Make the simulator
+export PATH=$CUDA_INSTALL_PATH/bin:$PATH;
+
 source src/setup_environment.sh
 make -j -C src/
