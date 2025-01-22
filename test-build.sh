f2ea6fb5019cce18572f95dcb2a3d15ee3506989
tgrogers <tgrogers@purdue.edu>
2025-01-22 18:43:47 -0500

    Installing boost, and setting the path, also fixing some odd whitespace stuff in the 12.6 build

--- test-build.sh
+++ test-build.sh
@@ -3,8 +3,13 @@
 set -e
 
 if [ ! -n "$CUDA_INSTALL_PATH" ]; then
-»   echo "ERROR ** Install CUDA Toolkit and set CUDA_INSTALL_PATH.";
-»   exit;
+    echo "ERROR ** Install CUDA Toolkit and set CUDA_INSTALL_PATH.";
+    exit;
+fi
+
+if [ ! -n "$BOOST_ROOT" ]; then
+    echo "ERROR ** Install BOOST and set BOOST_ROOT.";
+    exit;
 fi
 
 #Make the simulator
