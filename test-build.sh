81b2ba83ea2408aceed161d1b5f87664c23aa08c
Tim Rogers <tgrogers@purdue.edu>
2025-02-15 19:14:41 -0500

    Update test-build.sh

--- test-build.sh
+++ test-build.sh
@@ -4,15 +4,14 @@ set -e
 
 if [ ! -n "$CUDA_INSTALL_PATH" ]; then
     echo "ERROR ** Install CUDA Toolkit and set CUDA_INSTALL_PATH.";
-    exit;
+    exit 1;
 fi
 
 if [ ! -n "$BOOST_ROOT" ]; then
     echo "ERROR ** Install BOOST and set BOOST_ROOT.";
-    exit;
+    exit 1;
 fi
 
-#Make the simulator
 export PATH=$CUDA_INSTALL_PATH/bin:$PATH;
 
 source src/setup_environment
