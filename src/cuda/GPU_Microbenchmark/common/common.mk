48bfbc17421e1021371337c5e7929341464cbeb8
Ahmad <aalawneh@purdue.edu>
2025-07-04 03:21:33 +0000

    fix include

--- src/cuda/GPU_Microbenchmark/common/common.mk
+++ src/cuda/GPU_Microbenchmark/common/common.mk
@@ -16,7 +16,7 @@ CUOPTS =  $(GENCODE_ARCH) $(GENCODE_SM50) $(GENCODE_SM60) $(GENCODE_SM62) $(GENC
 CC := nvcc
 
 # CUDA_PATH ?= /use/local/cuda-10.1/
-INCLUDE := $(BASE_DIR)/../../../../cuda-samples/cuda-samples/Common/
+INCLUDE := $(BASE_DIR)/../../../../cuda-samples/Common/
 LIB :=
 
 release:
