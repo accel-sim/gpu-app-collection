595a0bd6fb5d61196cb4e40d04ba11d109e013f6
Ahmad <aalawneh@purdue.edu>
2025-07-13 04:07:27 +0000

    fix lat

--- src/cuda/GPU_Microbenchmark/ubench/l2_cache/l2_lat/l2_lat.cu
+++ src/cuda/GPU_Microbenchmark/ubench/l2_cache/l2_lat/l2_lat.cu
@@ -1,19 +1,18 @@
 #include "../../l1_cache/l1_lat/l1_lat.h"
 #include "l2_lat.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 
- 
+  float lat2 = l2_hit_lat(argc, argv);
+  float lat1 = 0;
 
-  float lat2 = l2_hit_lat(argc,argv);
-
-
-    float lat1 = l1_lat(argc,argv);
-
-    std::cout << "\n//Accel_Sim config: \n";
-    std::cout << "-gpgpu_l2_rop_latency " << (unsigned)(lat2 - lat1)
-              << std::endl;
-  
+#ifdef TUNER
+  lat1 = l1_lat(argc, argv);
+#endif
+  std::cout << "\n//Accel_Sim config: \n";
+  std::cout << "-gpgpu_l2_rop_latency " << (unsigned)(lat2 - lat1)
+            << std::endl;
 
   return 1;
 }
--- src/cuda/GPU_Microbenchmark/ubench/mem/mem_lat/mem_lat.cu
+++ src/cuda/GPU_Microbenchmark/ubench/mem/mem_lat/mem_lat.cu
@@ -2,17 +2,16 @@
 #include "mem_lat.h"
 #include <iostream>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 
- 
-  float lat_mem = mem_lat(argc,argv);
-
-
-    float lat2 = l2_hit_lat(argc,argv);
-
-    std::cout << "\n//Accel_Sim config: \n";
-    std::cout << "-dram_latency " << (unsigned)(lat_mem - lat2) << std::endl;
- 
+  float lat_mem = mem_lat(argc, argv);
+  float lat2 = 0;
+#ifdef TUNER
+  lat2 = l2_hit_lat(argc, argv);
+#endif
+  std::cout << "\n//Accel_Sim config: \n";
+  std::cout << "-dram_latency " << (unsigned)(lat_mem - lat2) << std::endl;
 
   return 1;
 }
