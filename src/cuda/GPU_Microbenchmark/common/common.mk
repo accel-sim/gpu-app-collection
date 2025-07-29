4d271a0846efa593314a626a0ee6e5fca334db7c
root <aalawneh@purdue.edu>
2025-07-29 18:44:33 +0000

    add sm100 to tuner

--- src/cuda/GPU_Microbenchmark/common/common.mk
+++ src/cuda/GPU_Microbenchmark/common/common.mk
@@ -10,8 +10,10 @@ GENCODE_SM70 ?= -gencode=arch=compute_70,code=\"sm_70,compute_70\"
 GENCODE_SM75 ?= -gencode=arch=compute_75,code=\"sm_75,compute_75\"
 GENCODE_SM80 ?= -gencode=arch=compute_80,code=\"sm_80,compute_80\"
 GENCODE_SM86 ?= -gencode=arch=compute_86,code=\"sm_86,compute_86\"
+GENCODE_SM90 ?= -gencode=arch=compute_90,code=\"sm_90,compute_90\"
+GENCODE_SM100 ?= -gencode=arch=compute_100,code=\"sm_100,compute_100\"
 
-CUOPTS =  $(GENCODE_ARCH)  $(GENCODE_SM50) $(GENCODE_SM60) $(GENCODE_SM62) $(GENCODE_SM70) $(GENCODE_SM75) $(GENCODE_SM80)
+CUOPTS =  $(GENCODE_ARCH)  $(GENCODE_SM50) $(GENCODE_SM60) $(GENCODE_SM62) $(GENCODE_SM70) $(GENCODE_SM75) $(GENCODE_SM80) $(GENCODE_SM90) $(GENCODE_SM100)
 
 CC := nvcc
 
