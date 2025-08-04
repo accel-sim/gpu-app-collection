c2fdcc3e3d75eabffed6918da1e2245ee59f4b38
Ahmad <aalawneh@purdue.edu>
2025-08-04 19:21:17 +0000

    change ubench make

--- src/cuda/GPU_Microbenchmark/common/common.mk
+++ src/cuda/GPU_Microbenchmark/common/common.mk
@@ -1,19 +1,7 @@
 BASE_DIR := $(shell pwd)
 BIN_DIR := $(BASE_DIR)/../../../bin/
 
-GENCODE_SM30 ?= -gencode=arch=compute_30,code=\"sm_30,compute_30\"
-GENCODE_SM35 ?= -gencode=arch=compute_35,code=\"sm_35,compute_35\"
-GENCODE_SM50 ?= -gencode=arch=compute_50,code=\"sm_50,compute_50\"
-GENCODE_SM60 ?= -gencode=arch=compute_60,code=\"sm_60,compute_60\"
-GENCODE_SM62 ?= -gencode=arch=compute_62,code=\"sm_62,compute_62\"
-GENCODE_SM70 ?= -gencode=arch=compute_70,code=\"sm_70,compute_70\"
-GENCODE_SM75 ?= -gencode=arch=compute_75,code=\"sm_75,compute_75\"
-GENCODE_SM80 ?= -gencode=arch=compute_80,code=\"sm_80,compute_80\"
-GENCODE_SM86 ?= -gencode=arch=compute_86,code=\"sm_86,compute_86\"
-GENCODE_SM90 ?= -gencode=arch=compute_90,code=\"sm_90,compute_90\"
-GENCODE_SM100 ?= -gencode=arch=compute_100,code=\"sm_100,compute_100\"
-
-CUOPTS =  $(GENCODE_ARCH)  $(GENCODE_SM50) $(GENCODE_SM60) $(GENCODE_SM62) $(GENCODE_SM70) $(GENCODE_SM75) $(GENCODE_SM80) $(GENCODE_SM90) $(GENCODE_SM100)
+CUOPTS =  $(CUDA_CPPFLAGS)
 
 CC := nvcc
 
