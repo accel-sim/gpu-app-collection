afc2612bc419fea9139296054fe851683c73df3e
Ahmad Alawneh <lahmos4@gmail.com>
2025-09-18 19:06:31 +0000

    env enable

--- src/cuda/vllm/througput.sh
+++ src/cuda/vllm/througput.sh
@@ -1,6 +1,17 @@
 ENV_NAME="vllm-env"
 VLLM_REPO_DIR="vllm"
 
+
+if [ ! -d "./$ENV_NAME" ]; then
+    echo "You need to install vLLM first. Run install_vllm.sh"
+    exit 1
+fi
+export PATH="$PWD/$ENV_NAME/bin:$PATH"
+. $ENV_NAME/bin/activate
+if [ ! -d "$VLLM_REPO_DIR" ]; then
+    echo "You need to install vLLM first. Run install_vllm.sh"
+    exit 1
+fi
 FILE=ShareGPT_V3_unfiltered_cleaned_split.json
 if [ ! -f "$FILE" ]; then
     echo "$FILE does not exist. Downloading..."
@@ -15,19 +26,12 @@ else
     echo "$FILE already exists."
 fi
 
-if [ ! -d "./$ENV_NAME" ]; then
-    echo "You need to install vLLM first. Run install_vllm.sh"
-    exit 1
-fi
-export PATH="$PWD/$ENV_NAME/bin:$PATH"
-. $ENV_NAME/bin/activate
-if [ ! -d "$VLLM_REPO_DIR" ]; then
-    echo "You need to install vLLM first. Run install_vllm.sh"
-    exit 1
-fi
+
+
 
 MODEL_NAME="${1:-meta-llama/Meta-Llama-3.1-8B-Instruct}"
 
 
 
 vllm bench throughput  --enforce-eager --model "$MODEL_NAME" --tensor-parallel-size 1 --load-format dummy --dataset-path ./ShareGPT_V3_unfiltered_cleaned_split.json --num-prompts 50 --backend vllm
+deactivate
