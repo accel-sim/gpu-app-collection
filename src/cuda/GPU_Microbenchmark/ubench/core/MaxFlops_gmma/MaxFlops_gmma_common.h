8384fb3267a649178148c6c57fa2792e83d48d8d
Weili An <an107@purdue.edu>
2025-11-02 11:31:10 -0500

    update prints for MaxFlops_gmma

--- src/cuda/GPU_Microbenchmark/ubench/core/MaxFlops_gmma/MaxFlops_gmma_common.h
+++ src/cuda/GPU_Microbenchmark/ubench/core/MaxFlops_gmma/MaxFlops_gmma_common.h
@@ -190,8 +190,12 @@ float run_wgmma_maxflops_test_typed() {
   do { \
     try { \
       using TileShape = decltype(make_shape(Int<M>{}, Int<N>{}, Int<K>{})); \
-      float inst_throughput = run_wgmma_maxflops_test_typed<EA, EB, EC, TileShape>(); \
-      printf("%-50s: %6.2f warp instructions/cycle\n", DESC, inst_throughput); \
+      float num_flop_per_warpgroup = 2 * M * N * K; \
+      float warp_inst_per_cycle = run_wgmma_maxflops_test_typed<EA, EB, EC, TileShape>(); \
+      const float warps_to_warpgroup = 1/4; \
+      float flop_per_cycle_per_warpgroup = num_flop_per_warpgroup * warp_inst_per_cycle * warps_to_warpgroup; \
+      printf("%-50s: %6.4f warp instructions/cycle\n", DESC, warp_inst_per_cycle); \
+      printf("%-50s: %6.4f flop/warpgroup inst/cycle\n", DESC, flop_per_cycle_per_warpgroup); \
     } catch (...) { \
       printf("%-50s: FAILED\n", DESC); \
     } \
