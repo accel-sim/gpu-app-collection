cb832b92a7c8b630da63cd02dfa73e4b31e24e39
Weili An <an107@purdue.edu>
2026-01-08 13:31:13 -0500

    Add test binaries for GMMA instruction

---
+++ src/cuda/GPU_Microbenchmark/ubench/core/lat_gmma/lat_gmma_test.cu
@@ -0,0 +1,10 @@
+#include <cuda.h>
+
+void run_f32bf16bf16_64x256x16_test();
+
+int main()
+{
+  // Simple a test kernel to check if the library is working
+  run_f32bf16bf16_64x256x16_test();
+  return 0;
+}
